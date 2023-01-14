28c15f1e3afd40b4a773b820bdb234930d568b04
Vijender <Vijendersingh2507@gmail.com>
2023-01-14 11:26:30 +0530

    aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
-import TopBar from './components/TopBar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import TopBar from "./components/TopBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <TopBar/>
-      <Header />
+      {/* <TopBar/> */}
+      {/* <Header /> */}
       <Sidebar />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../assets/images/svg/Logo.svg";
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <>
+      <section>
+        <aside className="mt-5 pt-5">
+          <a href="#">
+            <img src={Logo} alt="Main champaign" />
+          </a>
+        </aside>
+      </section>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
