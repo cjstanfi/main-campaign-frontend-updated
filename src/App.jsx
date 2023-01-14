e8ef390da9baf7657272a3110fb6aa665966e7cf
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-14 11:24:33 +0530

    install react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import TopBar from './components/TopBar'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <TopBar/>
       <Header />
       <Sidebar />
-      
     </>
   )
 }
