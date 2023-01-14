4df1e9d340b1454098752514daba9180a240c400
Vijender <Vijendersingh2507@gmail.com>
2023-01-14 11:40:10 +0530

    pull

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import Logo from "../assets/images/svg/Logo.svg";
+
 const Sidebar = () => {
-  return (
-    <>
-      <section>
-        <aside className="mt-5 pt-5">
-          <a href="#">
-            <img src={Logo} alt="Main champaign" />
-          </a>
-        </aside>
-      </section>
-    </>
-  );
+  return <div>Sidebar</div>;
 };
 
 export default Sidebar;
