1af39420a92aa2815d64f0dff1a1c0177f6a9008
Vijender <Vijendersingh2507@gmail.com>
2023-01-14 13:45:06 +0530

    header and topbar section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <section>
+        <div className="container">
+          <div className="d-flex overflow-auto">
+            <a
+              className="text-decoration-none ff_thicccboi_bold font-xl colort_blue"
+              href="#"
+            >
+              Profile
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Facebook
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              CRM
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Business
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Privacy
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Billing
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Team
+            </a>
+            <a
+              className="text-decoration-none ff_thicccboi_medium font-xl colort_gray ms-5"
+              href="#"
+            >
+              Security
+            </a>
+          </div>
+          <hr className="height_2" />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Header
+export default Header;
