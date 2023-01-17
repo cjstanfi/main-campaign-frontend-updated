39e0a8e4064373e51b9f0830288921dea352c946
Vijender <Vijendersingh2507@gmail.com>
2023-01-17 16:35:44 +0530

    leadscenter

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
-import LeadsCenter from "../components/LeadsCenter";
 import Sidebar from "../components/Sidebar";
 import TopBar from "../components/TopBar";
 import Settings from "./Settings";
@@ -9,11 +8,10 @@ import Settings from "./Settings";
 const Dashboard = () => {
   return (
     <>
-      {/* <div className="d-flex">
+      <div className="d-flex">
         <Sidebar />
         <Outlet />
-      </div> */}
-      <LeadsCenter />
+      </div>
     </>
   );
 };
