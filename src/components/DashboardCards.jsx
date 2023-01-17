309ae25c25dc393b85af1e662c8a5cd9da246fd1
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-17 15:47:17 +0530

    make cards

---
+++ src/components/DashboardCards.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { cardsData } from "../components/common/Helper";
+import CardsGraphOne from "../assets/images/svg/cards-graph-1.svg";
+// import CardsGraphTwo from "../assets/images/svg/cards-graph-2.svg";
+const DashboardCards = () => {
+  return (
+    <>
+      <section className="py-5">
+        {/* <div className="row justify-content-between"> */}
+        <div className="d-flex">
+          {cardsData.map((val, index) => {
+            return (
+              <div key={index}>
+                <div className="dashboard_card_bg position-relative">
+                  <div className="position-absolute bottom-0">
+                    <img
+                      className="w-100"
+                      src={CardsGraphOne}
+                      alt="cards-graph"
+                    />
+                  </div>
+                  <div className="d-flex justify-content-between pt-3 ps-3">
+                    <div className="d-flex justify-content-between pt-1">
+                      <h2 className="font-xl fw_600 color_gray ff_thicccboi_semibold pt-1">
+                        {val.heading}
+                      </h2>
+                      <div className="ps-2">
+                        <img src={val.imgUrl} alt="cards-icon" />
+                      </div>
+                    </div>
+                    <div className="cards_text_bg me-2">
+                      <p className="mb-0 font-sm fw_500 ff_thicccboi_medium color_green">
+                        {val.subheading}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="font-2xl fw_600 color_dark_black ff_thicccboi_semibold ps-3">
+                    {val.amount}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default DashboardCards;
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,34 @@
+import ToolTipIcon from "../../assets/images/svg/cards-icon.svg";
+
+export const cardsData = [
+  {
+    heading: "Total Revenue",
+    subheading: "+1.5%",
+    amount: "$25,000",
+    imgUrl: ToolTipIcon,
+  },
+  {
+    heading: "Total Spend",
+    subheading: "+1.5%",
+    amount: "$1,200",
+    imgUrl: ToolTipIcon,
+  },
+  {
+    heading: "Total Impression",
+    subheading: "+1.5%",
+    amount: "250.45K",
+    imgUrl: ToolTipIcon,
+  },
+  {
+    heading: "Total Reached",
+    subheading: "+2.25%",
+    amount: "50.2k",
+    imgUrl: ToolTipIcon,
+  },
+  {
+    heading: "Total Clicks",
+    subheading: "-0.5%",
+    amount: "45.5k",
+    imgUrl: ToolTipIcon,
+  },
+];
