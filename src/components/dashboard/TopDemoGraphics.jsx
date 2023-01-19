a28b5ba408c3fa9a1a552b3e5f702c6f3d857c2d
sharukhk08 <iamsharukh08@gmail.com>
2023-01-19 10:04:09 +0530

    color fixed

--- src/components/dashboard/TopDemoGraphics.jsx
+++ src/components/dashboard/TopDemoGraphics.jsx
@@ -4,12 +4,12 @@ import { Chart } from "react-google-charts";
 const TopDemoGraphics = () => {
   const data = [
     ["Country", "Leads"],
-    ["Germany", 200],
-    ["United States", 800],
-    ["Brazil", 400],
-    ["Australia", 500],
+    ["Germany", 600],
+    ["United States", 600],
+    ["Brazil", 600],
+    ["Australia", 600],
     ["France", 600],
-    ["India", 400],
+    ["India", 600],
   ];
   const options = {
     colors: ["#3B5998"],
