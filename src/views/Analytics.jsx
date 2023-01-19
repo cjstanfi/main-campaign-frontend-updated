83ead55432a2bb60aea3be07f4f674c6cd428021
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-19 10:47:21 +0530

    make dropdown data

--- src/views/Analytics.jsx
+++ src/views/Analytics.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LeadsDropdownTabs from "../components/LeadsDropdownTabs";
 
 const Analytics = () => {
-  return <div>Analytics</div>;
+  return (
+    <>
+      <LeadsDropdownTabs />
+    </>
+  );
 };
 
 export default Analytics;
