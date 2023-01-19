86e97fdcab1a21276d0dc70bc1e65c71661f452a
neharadialcode <neharadialcode@gmail.com>
2023-01-19 14:55:26 +0530

    dropdown

--- src/components/LeadsDropdownTabs.jsx
+++ src/components/LeadsDropdownTabs.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
+import React, { useState } from "react";
 import { PlusIcon } from "./common/icons";
 const LeadsDropdownTabs = () => {
+  const [hello, setHello] = useState(false);
   return (
     <>
-      <section>
-        <div className="pt-4 pb-3 text-nowrap overflow_x_auto_dropdown">
+      <section className="">
+        <div className="pt-4 pb-3 text-nowrap over_y_hidden overflow_x_auto_dropdown  ">
           <button className="font-sm fw_600 ff_thicccboi_semibold color_white leads_filter_btn text-nowrap">
             + Add Filters
           </button>
-          <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ms-3 ms-sm-4">
-            <Dropdown>
-              <Dropdown.Toggle
-                variant="success"
-                id="dropdown-basic"
-                className="bgtransparent border-0 p-0"
-              >
-                All Forms
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </button>
+
+          <button onClick={() => setHello(!hello)}>Hello</button>
+          {hello ? (
+            <div className="position-absolute bg-danger">
+              <p>Lorem ipsum</p>
+              <p>Lorem ipsum</p>
+              <p>Lorem ipsum</p>
+              <p>Lorem ipsum</p>
+              <p>Lorem ipsum</p>
+            </div>
+          ) : (
+            ""
+          )}
+
           <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ms-3 ms-sm-4">
             All Owners
           </button>
+
           <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ms-3 ms-sm-4">
             Select Dates
           </button>
+
           <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ms-3 ms-sm-4">
             All Lable
           </button>
+
           <button className="font-sm fw_600 ff_thicccboi_semibold color_gray border-0 bg-transparent text-nowrap ms-3 ms-sm-4">
             <span className="into_icon d-inline-block me-1">
               <PlusIcon />
