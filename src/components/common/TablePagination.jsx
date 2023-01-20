e69cda1bdc06a7b0e6912fbedfc8b4849ac27fa6
Vijender <Vijendersingh2507@gmail.com>
2023-01-20 16:13:03 +0530

    table pagination

--- src/components/common/TablePagination.jsx
+++ src/components/common/TablePagination.jsx
@@ -14,8 +14,10 @@ const TablePagination = () => {
                     ? "table_tabs_btn_active"
                     : index === TableTabs.length - 1
                     ? "table_tabs_btn_active"
+                    : index === 2 || index === 3 || index === 4
+                    ? "d-none d-md-inline"
                     : "table_tabs_btn"
-                } me-3 px-3 py-2`}
+                } me-3 px-3 py-2 table_tabs_btn`}
                 key={index}
               >
                 {item.pageNo}
