d41ba46819d958e662e0ff87e3fdab8d40e4377c
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-20 15:39:54 +0530

    chng in topbar dropdown

--- src/components/common/TopBar.jsx
+++ src/components/common/TopBar.jsx
@@ -3,26 +3,26 @@ import { SettingIcon, NotificationIcon } from "./Icons";
 import UserIcon from "../../assets/images/png/User_icon.png";
 import UserIconTick from "../../assets/images/svg/User_icon_tick.svg";
 import Dropdown from "react-bootstrap/Dropdown";
+import DashBoardCalendarly from "../dashboard/DashBoardCalendarly";
 const TopBar = ({ heading, para }) => {
   return (
     <>
       <section className="pt-3 pt-lg-0 position-relative">
-        <div className="d-sm-flex pt-lg-5 pb-4 justify-content-between align-items-center">
+        <div className="d-xxl-flex pt-lg-5 pb-lg-4 justify-content-between align-items-center">
           <div>
             <h3 className="font-3xl color_dark_black fw_600 mb-1 ff_thicccboi_semibold">
               {heading}
             </h3>
             {/* <p className="font-md color_gray fw_400 mb-0">{para}</p> */}
-            {/* <div className="d-flex">
+            <div className="d-sm-flex pt-2 pt-lg-0">
               <Dropdown>
                 <Dropdown.Toggle
                   variant="success"
                   id="dropdown-basic"
-                  className="font-md fw-normal color_gray bg-transparent dropdown_tab"
+                  className="font-md fw-normal color_gray bg-transparent dropdown_tab py-2"
                 >
                   The Place Fitness (0126589526)
                 </Dropdown.Toggle>
-
                 <Dropdown.Menu>
                   <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                   <Dropdown.Item href="#/action-2">
@@ -33,45 +33,40 @@ const TopBar = ({ heading, para }) => {
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
-              <button className="ff_thicccboi_semibold ms-2 border-0 text-white save_btn px-4 py-2 ">
+              <button className="ff_thicccboi_semibold font-sm ms-sm-2 border-0 text-white save_btn px-4 py-2 mt-2 mt-sm-0">
                 + Add a Business
               </button>
-            </div> */}
+            </div>
           </div>
-          <div className="d-flex align-items-center pt-3 pt-sm-0 ">
-            {/* <div className="d-flex align-items-center">
-              <div>
-                <input className="date_input" type="date" />
-              </div>
-              <div className="px-3">
-                <input className="date_input" type="date" />
-              </div>
-            </div> */}
-            <a
-              href="#"
-              className="icon_bg_color border_r_12 cursor_pointer icon_size d-flex align-items-center justify-content-center"
-            >
-              <SettingIcon />
-            </a>
-            <a
-              href="#"
-              className="icon_bg_color border_r_12 cursor_pointer icon_size d-flex align-items-center justify-content-center ms-3"
-            >
-              <NotificationIcon />
-            </a>
-            <div className="position-relative pe_12">
-              <a href="#">
-                <img
-                  className="user_icon user_icon_size"
-                  src={UserIcon}
-                  alt="Icon"
-                />
-                <img
-                  className="position-absolute top-50 translate-middle-x"
-                  src={UserIconTick}
-                  alt="Verified"
-                />
+          <div className="d-md-flex align-items-center pt-3 pt-xxl-0">
+            <DashBoardCalendarly />
+            <div className="d-flex mt-3 mt-md-1">
+              <a
+                href="#"
+                className="icon_bg_color border_r_12 cursor_pointer icon_size d-flex align-items-center justify-content-center"
+              >
+                <SettingIcon />
               </a>
+              <a
+                href="#"
+                className="icon_bg_color border_r_12 cursor_pointer icon_size d-flex align-items-center justify-content-center ms-3"
+              >
+                <NotificationIcon />
+              </a>
+              <div className="position-relative pe_12">
+                <a href="#">
+                  <img
+                    className="user_icon user_icon_size"
+                    src={UserIcon}
+                    alt="Icon"
+                  />
+                  <img
+                    className="position-absolute top-50 translate-middle-x"
+                    src={UserIconTick}
+                    alt="Verified"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
