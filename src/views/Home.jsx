1e1ab37fdebbb6f8c71f8dd8f766741af99ca3ad
Surender-147 <surendersingh9728511667@gmail.com>
2023-01-21 13:50:27 +0530

    routes fixed

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../components/landing/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
