7b1efdcd8b901b87a2c3a8d7d7959c53864ca1b8
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-21 11:10:55 +0530

    make tooltip

--- src/components/dashboard/DashBoardCalendarly.jsx
+++ src/components/dashboard/DashBoardCalendarly.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DashBoardCalendarly = () => {
+  const [inputValue, setInputValue] = useState("2022-07-01");
+  const [inputValueTwo, setInputValueTwo] = useState("2023-07-01");
   return (
     <>
       <div className="d-flex align-items-center">
         <div>
-          <input className="date_input" type="date" />
+          <input
+            className="date_input"
+            type="date"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </div>
         <div className="ps-3 px-md-3">
-          <input className="date_input" type="date" />
+          <input
+            className="date_input"
+            type="date"
+            value={inputValueTwo}
+            onChange={(e) => setInputValueTwo(e.target.value)}
+          />
         </div>
       </div>
     </>
