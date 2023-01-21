5f54c122487e86052fe3a4615b517f518a5d6b4a
neharadialcode <neharadialcode@gmail.com>
2023-01-21 11:43:24 +0530

    tabs added

--- src/views/Analytics.jsx
+++ src/views/Analytics.jsx
@@ -28,15 +28,15 @@ const Analytics = () => {
             </div>
             <TotalLeads />
           </div>
-          <div className="col-lg-4 border_left_1px p-4">
+          <div className="col-lg-4 border_left_1px p-xl-4 p-3 my-lg-3">
             <DashboardCost />
           </div>
         </div>
-        <div className="row mb-4 mx-0 total-leads-wrapper mt-4 justify-content-center">
+        <div className="row mb-4 mx-0 total-leads-wrapper mt-4 ">
           <div className="col-lg-8  d-flex flex-column justify-content-center align-item-center">
             <TopDemoGraphics />
           </div>
-          <div className="col-lg-4 col-sm-8 pb-3 border_left_1px d-flex flex-column justify-content-center">
+          <div className="col-lg-4 col-sm-10 pb-3 my-lg-3 border_left_1px d-flex flex-column justify-content-center">
             <AgeGroupBarChart />
           </div>
         </div>
