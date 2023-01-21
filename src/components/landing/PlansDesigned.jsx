1847ec998449e7da86eccf6f64ef410e8765c0fb
Surender-147 <surendersingh9728511667@gmail.com>
2023-01-21 16:08:34 +0530

    add scake

---
+++ src/components/landing/PlansDesigned.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const Plansdesigned = () => {
+  return (
+    <>
+      <section className="my-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-10">
+              <div className="text-center">
+                <h2 className="sub_heading mb-5 pb-4">
+                  Plans designed <br /> for you to grow.
+                </h2>
+                <div className="d-flex">
+                  <div className="col-6">
+                    <div className="plans_cards text-start">
+                      <h3 className="font-4xl fw-bold ff_thicccboi_bold color_dark_black mb-3 pb-1">
+                        small business
+                      </h3>
+                      <h3 className="font-2xl fw_semibold ff_thicccboi_bold color_dark_black">
+                        $199 <span className="color_blue fs_22 ">/ month</span>
+                      </h3>
+                      <ul className="list-unstyled mt-4 pt-2">
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black">
+                          1 CRM & marketing platform integration
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Accurate campaign lead data
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Full access to reports
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Unlimited team members
+                        </li>{" "}
+                        <div className="text-center mt-5 pt-5">
+                          <button className="leads_btn border-0 font-md fw_md ff_thicccboi_medium color_white my-3 my-sm-4">
+                            Start My Free Trial
+                          </button>
+                        </div>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="plans_cards text-start">
+                      <h3 className="font-4xl fw-bold ff_thicccboi_bold color_dark_black mb-3 pb-1">
+                        small business
+                      </h3>
+                      <h3 className="font-2xl fw_semibold ff_thicccboi_bold color_dark_black">
+                        $199 <span className="color_blue fs_22 ">/ month</span>
+                      </h3>
+                      <ul className="list-unstyled mt-4 pt-2">
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black">
+                          1 CRM & marketing platform integration
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Accurate campaign lead data
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Full access to reports
+                        </li>
+                        <li className="font-lg  fw_md ff_thicccboi_medium color_dark_black mt-3">
+                          Unlimited team members
+                        </li>{" "}
+                        <div className="text-center mt-5 pt-5">
+                          <button className="leads_btn border-0 font-md fw_md ff_thicccboi_medium color_white my-3 my-sm-4">
+                            Start My Free Trial
+                          </button>
+                        </div>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Plansdesigned;
