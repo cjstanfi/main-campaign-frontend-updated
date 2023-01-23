ed33bec330c241fac4bad8cd5ebc9e244ba75f32
Surender-147 <surendersingh9728511667@gmail.com>
2023-01-23 16:59:40 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./assets/css/landing.css";
 import AppRoutes from "./AppRoutes";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <>
