f6a77ffa7f6e4a6439e8fafdc338567f36d5ca4d
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-01-23 14:37:28 +0530

    chng in drowpdown to select

--- src/components/leadscenter/LeadsCenterContactInfo.jsx
+++ src/components/leadscenter/LeadsCenterContactInfo.jsx
@@ -2,62 +2,62 @@ import React, { useState } from "react";
 import { LeadsCenterContactDetails } from "../common/Helper";
 import { ThreeDots } from "../common/Icons";
 import LeadsDropdownTabs from "./LeadsDropdownTabs";
-// import Dropdown from "react-bootstrap/Dropdown";
+import Dropdown from "react-bootstrap/Dropdown";
 const LeadsCenterContactInfo = () => {
   const [showtab, setShowtab] = useState(LeadsCenterContactDetails[0]);
   return (
     <>
-      <>
         <section className="mt-4">
-          <div className="d-flex overflow-auto pb-3 justify-content-between align-items-center">
-            <div className="d-flex ">
-              {LeadsCenterContactDetails &&
-                LeadsCenterContactDetails.map((item, index) => {
-                  return (
-                    <a
-                      key={index}
-                      onClick={() => setShowtab(item)}
-                      className={`${
-                        item == showtab
-                          ? "tab_border color_blue "
-                          : "header_tabs"
-                      } text-decoration-none ff_thicccboi_medium font-xl color_gray position-relative cursor_pointer me-4 me-sm-5 text-nowrap`}
-                    >
-                      {item.tabHeading}{" "}
-                      <span
-                        className={
-                          item.noOfLeads
-                            ? "color_dark_black font-sm ff_thicccboi_medium ms-2 py-1 px-2 bg_light_grey"
-                            : "d-none"
-                        }
+          <div className="d-flex">
+            <div className="d-flex overflow-auto pb-3 justify-content-between align-items-center">
+              <div className="d-flex">
+                {LeadsCenterContactDetails &&
+                  LeadsCenterContactDetails.map((item, index) => {
+                    return (
+                      <a
+                        key={index}
+                        onClick={() => setShowtab(item)}
+                        className={`${
+                          item == showtab
+                            ? "tab_border color_blue "
+                            : "header_tabs"
+                        } text-decoration-none ff_thicccboi_medium font-xl color_gray position-relative cursor_pointer me-4 me-sm-5 text-nowrap`}
                       >
-                        {item.noOfLeads}
-                      </span>
-                      <span
-                        className={
-                          item.arrow && showtab === item
-                            ? "leads_center_arrow_icon ms-4"
-                            : item.arrow
-                            ? "ms-4 "
-                            : "d-none"
-                        }
-                      >
-                        {item.arrow}
-                      </span>
-                    </a>
-                  );
-                })}
+                        {item.tabHeading}{" "}
+                        <span
+                          className={
+                            item.noOfLeads
+                              ? "color_dark_black font-sm ff_thicccboi_medium ms-2 py-1 px-2 bg_light_grey"
+                              : "d-none"
+                          }
+                        >
+                          {item.noOfLeads}
+                        </span>
+                        <span
+                          className={
+                            item.arrow && showtab === item
+                              ? "leads_center_arrow_icon ms-4"
+                              : item.arrow
+                              ? "ms-4 "
+                              : "d-none"
+                          }
+                        >
+                          {item.arrow}
+                        </span>
+                      </a>
+                    );
+                  })}
+              </div>
             </div>
             <div>
               <span className="cursor_pointer">
-                <ThreeDots />
-                {/* <Dropdown>
+                <Dropdown className="three_dot_dropdown ps-2 ps-xxl-0">
                   <Dropdown.Toggle
                     variant="success"
                     id="dropdown-basic"
-                    className="bg-transparent border-0 p-0 color_gray"
+                    className="bg-transparent border-0 p-0 color_gray "
                   >
-                    All Forms
+                    <ThreeDots />
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
@@ -68,7 +68,7 @@ const LeadsCenterContactInfo = () => {
                       Something else
                     </Dropdown.Item>
                   </Dropdown.Menu>
-                </Dropdown> */}
+                </Dropdown>
               </span>
             </div>
           </div>
@@ -77,7 +77,6 @@ const LeadsCenterContactInfo = () => {
         </section>
         <div className="mb-3">{showtab.content}</div>
       </>
-    </>
   );
 };
 
--- src/components/leadscenter/LeadsDropdownTabs.jsx
+++ src/components/leadscenter/LeadsDropdownTabs.jsx
@@ -1,16 +1,122 @@
 import React, { useState } from "react";
-import { PlusIcon } from "../common/Icons";
-import Dropdown from "react-bootstrap/Dropdown";
+import { PlusIcon, SelectIcon } from "../common/Icons";
+// import Dropdown from "react-bootstrap/Dropdown";
 const LeadsDropdownTabs = () => {
-  // const [hello, setHello] = useState(false);
+  const [showvalue, setShowvalue] = useState("1999-11-30");
+  const leadsformsData = [
+    {
+      value: "All Forms",
+    },
+    {
+      value: "Forms One",
+    },
+    {
+      value: "Forms Two",
+    },
+  ];
+  const leadsOwnerData = [
+    {
+      value: "All Owners",
+    },
+    {
+      value: "Owners One",
+    },
+    {
+      value: "Owners Two",
+    },
+  ];
+  const leadsLableData = [
+    {
+      value: "All Lable",
+    },
+    {
+      value: "Lable One",
+    },
+    {
+      value: "Lable Two",
+    },
+  ];
+  const [show, setShow] = useState(false);
+  const handleChangeTabs = (obj) => {
+    setShow(false);
+    setSelectedItem(obj);
+  };
+  const [ownershow, setOwnershow] = useState(false);
+  const handleChangeTabsOwner = (obj) => {
+    setOwnershow(false);
+    setSelectedItemTwo(obj);
+  };
+  const [lable, setLable] = useState(false);
+  const handleChangeTabsLable = (obj) => {
+    setLable(false);
+    setSelectedItemThree(obj);
+  };
+  const [selectedItem, setSelectedItem] = useState(leadsformsData[0]);
+  const [selectedItemTwo, setSelectedItemTwo] = useState(leadsOwnerData[0]);
+  const [selectedItemThree, setSelectedItemThree] = useState(leadsLableData[0]);
   return (
     <>
-      <section className="">
+      <section>
         <div className=" pb-3 over_y_hidden overflow_x_auto_dropdown leads_dropdown d-flex align-items-center">
           <button className="font-sm fw_600 ff_thicccboi_semibold color_white leads_filter_btn text-nowrap">
             + Add Filters
           </button>
-          <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
+
+          <div className="custom_select">
+            <h4
+              className="mb-0 font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap"
+              onClick={() => setShow(!show)}
+            >
+              {selectedItem.value}
+              <span className={`select_arrow ${show && "rotate_180"} ps-2`}>
+                <SelectIcon />
+              </span>
+            </h4>
+            {show && (
+              <ul className="mb-0 list-unstyled font-sm">
+                {leadsformsData &&
+                  leadsformsData.map((obj, index) => (
+                    <li
+                      className="py-2 px-3 font-sm"
+                      onClick={() => handleChangeTabs(obj)}
+                      key={index}
+                    >
+                      {obj.value}
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
+
+          <div className="custom_select">
+            <h4
+              className="mb-0 font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap"
+              onClick={() => setOwnershow(!ownershow)}
+            >
+              {selectedItemTwo.value}
+              <span
+                className={`select_arrow ${ownershow && "rotate_180"} ps-2 `}
+              >
+                <SelectIcon />
+              </span>
+            </h4>
+            {ownershow && (
+              <ul className="mb-0 list-unstyled font-sm">
+                {leadsOwnerData &&
+                  leadsOwnerData.map((obj, index) => (
+                    <li
+                      className="py-2 px-3 font-sm"
+                      onClick={() => handleChangeTabsOwner(obj)}
+                      key={index}
+                    >
+                      {obj.value}
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
+
+          {/* <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
             <Dropdown>
               <Dropdown.Toggle
                 variant="success"
@@ -25,9 +131,9 @@ const LeadsDropdownTabs = () => {
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </button>
+          </button> */}
 
-          <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
+          {/* <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
             <Dropdown>
               <Dropdown.Toggle
                 variant="success"
@@ -42,9 +148,42 @@ const LeadsDropdownTabs = () => {
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </button>
+          </button> */}
 
-          <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
+          <input
+            onChange={(e) => setShowvalue(e.target.value)}
+            value={showvalue}
+            type="date"
+            placeholder="Select Dates"
+            className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap calendar_input"
+          />
+
+          <div className="custom_select">
+            <h4
+              className="mb-0 font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap"
+              onClick={() => setLable(!lable)}
+            >
+              {selectedItemThree.value}
+              <span className={`select_arrow ${lable && "rotate_180"} ps-2 `}>
+                <SelectIcon />
+              </span>
+            </h4>
+            {lable && (
+              <ul className="mb-0 list-unstyled font-sm">
+                {leadsLableData &&
+                  leadsLableData.map((obj, index) => (
+                    <li
+                      className="py-2 px-3 font-sm"
+                      onClick={() => handleChangeTabsLable(obj)}
+                      key={index}
+                    >
+                      {obj.value}
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
+          {/* <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
             <Dropdown>
               <Dropdown.Toggle
                 variant="success"
@@ -54,14 +193,14 @@ const LeadsDropdownTabs = () => {
                 Select Dates
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-1">
+                </Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </button>
-
-          <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
+          </button> */}
+          {/* <button className="font-sm fw_600 ff_thicccboi_semibold color_gray leads_dropdown_btn text-nowrap ">
             <Dropdown>
               <Dropdown.Toggle
                 variant="success"
@@ -76,7 +215,7 @@ const LeadsDropdownTabs = () => {
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </button>
+          </button> */}
 
           <button className="font-sm fw_600 ff_thicccboi_semibold color_gray border-0 bg-transparent text-nowrap ">
             <span className="into_icon d-inline-block me-1">
